14005e0a7637183c82d184fda10aeb4e25f87c6a
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-03-14 20:03:01 +0530

    register, login, and logout backend created

--- backend/Server.js
+++ backend/Server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const adminModel = require("./Admins");
-// const userModel = require("./Courses");
+const userModel = require("./Admins");
 
 const PORT = 3000 || process.env.PORT;
 
@@ -14,37 +14,37 @@ app.use(cors());
 
 //login logout and register functions for user
 
-// const register = async(req,res) => {
-//     try {
-//         const user = new userModel({
-//             username: req.body.username,
-//             password: req.body.password
-//         });
-//         await user.save();
-//         res.send('User registered successfully');
-//     } catch (error) {
-//         res.send("Error creating User");
-//     }
-// }
+const register = async(req,res) => {
+    try {
+        const user = new userModel({
+            username: req.body.username,
+            password: req.body.password
+        });
+        await user.save();
+        res.send('User registered successfully');
+    } catch (error) {
+        res.send("Error creating User");
+    }
+}
 
-// const login = async(req,res) => {
-//     const {username, password} = req.body;
-//     try {
-//         const user = await userModel.findOne({username});
-//         if(!user){
-//             res.send("No User found");
-//         }
-//         else{
-//             res.status(200).json(user);
-//         }
-//     } catch (error) {
-//         res.status(404).json({message: "Error logging in"});
-//     }
-// }
+const login = async(req,res) => {
+    const {username, password} = req.body;
+    try {
+        const user = await userModel.findOne({username});
+        if(!user){
+            res.send("No User found");
+        }
+        else{
+            res.status(200).json(user);
+        }
+    } catch (error) {
+        res.status(404).json({message: "Error logging in"});
+    }
+}
 
-// const logout = async(req,res) => {
+const logout = async(req,res) => {
     
-// }
+}
 
 //crud functions for admin
 
@@ -115,6 +115,9 @@ app.put("/:id", updateAdminById);
 app.get("/:id", getCoursesbyId);
 
 //All user routes
+app.post("/register", register);
+app.get("/login", login);
+app.post("/logout", logout);
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
